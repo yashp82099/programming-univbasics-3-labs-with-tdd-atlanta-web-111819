684ec889f6fe3697006d17ac3ca1c440bedfeaab
yash patel <yash82099@yahoo.com>
2019-10-10 17:06:10 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,7 @@
-# Add your variables here
+Integer first_number = 8;
+Integer second_number = 4;
+
+Integer sum = first_number + second_number;
+Integer difference = first_number - second_number;
+Integer product = first_number * second_number;
+Integer quotient = first_number / second_number;
