f112b4ff664d0dac90ddade483bfe3602bf8767f
yash patel <yash82099@yahoo.com>
2019-10-10 17:03:11 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 Integer = first_number;
 Integer = second_number;
 
-first_number + second_number;
-first_number - second_number;
-first_number * second_number;
-first_number / second_number;
+Integer sum = first_number + second_number;
+Integer difference = first_number - second_number;
+Integer product = first_number * second_number;
+Integer quotient = first_number / second_number;
