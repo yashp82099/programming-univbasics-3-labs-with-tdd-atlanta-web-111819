16486c5a47cb11cdab756e4f6567362213dfbe77
yash patel <yash82099@yahoo.com>
2019-10-10 17:01:25 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,7 @@
 Integer = first_number;
 Integer = second_number;
 
-sum = first_number + second_number;
-difference = first_number - second_number;
+first_number + second_number;
+first_number - second_number;
+first_number * second_number;
+first_number / second_number;
